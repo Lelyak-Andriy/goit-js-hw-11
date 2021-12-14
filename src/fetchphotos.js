b5e90406c4bea3dbe7a87be6894d725ab6fbd40f
Lelyak-Andriy <a.lelyak47@gmail.com>
2021-12-14 23:38:46 +0200

    354

--- src/fetchphotos.js
+++ src/fetchphotos.js
@@ -1,15 +1,16 @@
 export default {
+    
     apiKey: '24769515-7151fb4d93d5940551af86767',
-    baseUrl: 'https://pixabay.com/api',
+    baseUrl: 'https://pixabay.com/api/',
     searchQuery: '',
-    perPage: 20,
+    perPage: 4,
     page: 1,
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true,
 
     fetchPhotos() {
-        const url = `${this.baseUrl}/?q=${this.query}&image_type=photo&orientation=horizontal&per_page=${this.perPage}&page=${this.page}&key=${this.apiKey}`;
+        const url = `${this.baseUrl}?q=${this.query}&image_type=photo&orientation=horizontal&per_page=${this.perPage}&page=${this.page}&key=${this.apiKey}`;
         return fetch(url)
             .then(response => {
                 this.incrementPage();
--- src/index.js
+++ src/index.js
@@ -1,36 +1,78 @@
 import './css/styles.css';
 import galleryItem from './templates/galleryList.hbs';
 import apiService from './fetchPhotos';
-// import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 
 const searchForm = document.querySelector(".search-form");
 const galleryList = document.querySelector(".gallery");
 const btnLoadMore = document.querySelector(".load-more");
 
 searchForm.addEventListener('submit', onSubmit);
-btnLoadMore.addEventListener('click', onLoadMore);
+btnLoadMore.addEventListener('click', fetchPhotos);
 
+
+    hideButton();
+    disableButton();
+    
 
 function onSubmit(e) {
     e.preventDefault();
     
     apiService.query = e.currentTarget.elements.searchQuery.value;
     if (apiService.query === "") {
-        galleryList.innerHTML = '';  
+        galleryList.innerHTML = '';
+        return Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.")
     }
+      
+    apiService.resetPage();
+    apiService.fetchPhotos().then(photos => {
+        clearGallery();
+        appendPhotoCard(photos)
         
-    // apiService.resetPage();
-    apiService.fetchPhotos().then(photos =>{});
+    });
 }
 
- 
-
-function onLoadMore() {
-apiService.fetchPhotos().then(appendPhotoCard);
+function fetchPhotos() {
+    hideButton();
+    disableButton();
+    apiService.fetchPhotos()
+.then(photos => {
+            appendPhotoCard(photos);
+        })
+.catch(error => console.log(error))
+.finally(() => {
+        enableButton()
+        showButton()
+})
 }
+
+
 
 function appendPhotoCard(photo) {
-    galleryList.insertAdjacentHTML('beforeend', galleryItem(photo.hits))
+galleryList.insertAdjacentHTML('beforeend', galleryItem(photo.hits))
+}
+
+function clearGallery() {
+galleryList.innerHTML = ''
+}
+
+function hideButton() {
+    btnLoadMore.classList.add("is-hidden");
+    btnLoadMore.innerHTML = '';
+}
+
+function showButton() {
+    btnLoadMore.classList.remove("is-hidden");
+}
+
+function disableButton() {
+    btnLoadMore.setAttribute("disabled", 'true');
+    btnLoadMore.textContent = 'Loading';
+}
+
+function enableButton() {
+    btnLoadMore.removeAttribute("disabled");
+    btnLoadMore.textContent = 'Load more';
 }
 
 
@@ -38,24 +80,3 @@ function appendPhotoCard(photo) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-// function clearGallery() {
-//     galleryList.innerHTML = '';
-// }
-
-
-
-// API.resetPage();
-    // API.fetchPhotos().then(photos => {
-    //     clearGallery();
-    //     appendPhotoCard(photos);
-    // });
