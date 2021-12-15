ed48c51095945cee0d552248fa58d5697cc508ab
Lelyak-Andriy <a.lelyak47@gmail.com>
2021-12-15 21:50:11 +0200

    аипвп

--- src/index.js
+++ src/index.js
@@ -2,68 +2,59 @@ import './css/styles.css';
 import galleryItem from './templates/galleryList.hbs';
 import apiService from './fetchPhotos';
 import Notiflix from 'notiflix';
-import LoadMoreBtn from './templates/loadmorebtn'
+
 
 const searchForm = document.querySelector(".search-form");
 const galleryList = document.querySelector(".gallery");
-// const loadMoreBtn = document.querySelector(".load-more");
+const btnLoadMore = document.querySelector(".load-more");
 
-const loadMoreBtn = new LoadMoreBtn({
-    selector: '[data-action="load-more"]',
-    hidden: true,
-});
 
-console.log(loadMoreBtn);
+console.log(btnLoadMore);
 
 searchForm.addEventListener('submit', onSubmit);
-// btnLoadMore.addEventListener('click', fetchPhotos);
-loadMoreBtn.refs.button.addEventListener('click', fetchPhotos);
+btnLoadMore.addEventListener('click', fetchPhotos);
 
-    // hideButton();
-    // disableButton();
-    
-    // loadMoreBtn.hide()
-    function onSubmit(e) {
-        e.preventDefault();
+if (apiService.query === "") {
+    hideButton();
+    disableButton();
+    clearGallery();
+}
+
+function onSubmit(e) {
+    e.preventDefault();
     
     apiService.query = e.currentTarget.elements.searchQuery.value;
     if (apiService.query === "") {
-        galleryList.innerHTML = '';
+        hideButton();
+        disableButton();
+        clearGallery();
         return Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.")
     }
-    loadMoreBtn.show();
-    apiService.resetPage();
     clearGallery();
+    apiService.resetPage();
     fetchPhotos();
     // apiService.fetchPhotos().then(photos => {
     //     clearGallery();
     //     appendPhotoCard(photos)
-        
     // });
 }
+//  до фетча все списав, продовжувати з функції фетч
 
 
 function fetchPhotos() {
-    loadMoreBtn.disable();
-    apiService.fetchPhotos().then(photos => {
-        appendPhotoCard(photos);
-        loadMoreBtn.enable();
-    })
-}
-// function fetchPhotos() {
-//     hideButton();
-//     disableButton();
-//     apiService.fetchPhotos()
-// .then(photos => {
-//             appendPhotoCard(photos);
+    // hideButton();
+    // disableButton();
+    apiService.fetchPhotos()
+.then(photos => {
+            appendPhotoCard(photos);
         
-// })
-// .catch(error => console.log(error))
-// .finally(() => {
-//         enableButton()
-//         showButton()
-// })
-// }
+})
+.catch(error => console.log(error))
+.finally(() => {
+        // enableButton()
+        // showButton()
+})
+}
 
 
 
@@ -75,24 +66,24 @@ function clearGallery() {
 galleryList.innerHTML = ''
 }
 
-// function hideButton() {
-//     btnLoadMore.classList.add("is-hidden");
-//     btnLoadMore.innerHTML = '';
-// }
+function hideButton() {
+    btnLoadMore.classList.add("is-hidden");
+    btnLoadMore.innerHTML = '';
+}
 
-// function showButton() {
-//     btnLoadMore.classList.remove("is-hidden");
-// }
+function showButton() {
+    btnLoadMore.classList.remove("is-hidden");
+}
 
-// function disableButton() {
-//     btnLoadMore.setAttribute("disabled", 'true');
-//     btnLoadMore.textContent = 'Loading';
-// }
+function disableButton() {
+    btnLoadMore.setAttribute("disabled", 'true');
+    btnLoadMore.textContent = 'Loading';
+}
 
-// function enableButton() {
-//     btnLoadMore.removeAttribute("disabled");
-//     btnLoadMore.textContent = 'Load more';
-// }
+function enableButton() {
+    btnLoadMore.removeAttribute("disabled");
+    btnLoadMore.textContent = 'Load more';
+}
 
 
 
